2afaf23c94dceeb5b86861d4ed223d88107f8c61
TheCong234 <127668626+TheCong234@users.noreply.github.com>
2023-07-23 11:14:10 +0700

    chuc nang cho fsetting

--- QuanLyQuanCafe/View/fSetting.Designer.cs
+++ QuanLyQuanCafe/View/fSetting.Designer.cs
@@ -28,92 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btLogout = new System.Windows.Forms.Button();
+            this.btnLanguage = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnAccount = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lbAcccountName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button7
+            // btnExit
             // 
-            this.button7.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_Thoat_32;
-            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button7.Location = new System.Drawing.Point(-2, 217);
-            this.button7.Margin = new System.Windows.Forms.Padding(0);
-            this.button7.Name = "button7";
-            this.button7.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.button7.Size = new System.Drawing.Size(205, 40);
-            this.button7.TabIndex = 5;
-            this.button7.Text = "Thoát";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnExit.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_Thoat_32;
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(-2, 217);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnExit.Size = new System.Drawing.Size(205, 40);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button8
+            // btLogout
             // 
-            this.button8.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_Exit_32;
-            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button8.Location = new System.Drawing.Point(-2, 177);
-            this.button8.Margin = new System.Windows.Forms.Padding(0);
-            this.button8.Name = "button8";
-            this.button8.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.button8.Size = new System.Drawing.Size(205, 40);
-            this.button8.TabIndex = 4;
-            this.button8.Text = "Đăng xuất";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btLogout.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_Exit_32;
+            this.btLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btLogout.Location = new System.Drawing.Point(-2, 177);
+            this.btLogout.Margin = new System.Windows.Forms.Padding(0);
+            this.btLogout.Name = "btLogout";
+            this.btLogout.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btLogout.Size = new System.Drawing.Size(205, 40);
+            this.btLogout.TabIndex = 4;
+            this.btLogout.Text = "Đăng xuất";
+            this.btLogout.UseVisualStyleBackColor = true;
+            this.btLogout.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button4
+            // btnLanguage
             // 
-            this.button4.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_Language_32;
-            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.Location = new System.Drawing.Point(-2, 113);
-            this.button4.Margin = new System.Windows.Forms.Padding(0);
-            this.button4.Name = "button4";
-            this.button4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.button4.Size = new System.Drawing.Size(205, 40);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Ngôn ngữ";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnLanguage.Enabled = false;
+            this.btnLanguage.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_Language_32;
+            this.btnLanguage.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLanguage.Location = new System.Drawing.Point(-2, 113);
+            this.btnLanguage.Margin = new System.Windows.Forms.Padding(0);
+            this.btnLanguage.Name = "btnLanguage";
+            this.btnLanguage.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnLanguage.Size = new System.Drawing.Size(205, 40);
+            this.btnLanguage.TabIndex = 4;
+            this.btnLanguage.Text = "Ngôn ngữ";
+            this.btnLanguage.UseVisualStyleBackColor = true;
+            this.btnLanguage.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button2
+            // btnSetting
             // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_settting_32;
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(-2, 73);
-            this.button2.Margin = new System.Windows.Forms.Padding(0);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.button2.Size = new System.Drawing.Size(205, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Cài đặt";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnSetting.BackColor = System.Drawing.Color.White;
+            this.btnSetting.Enabled = false;
+            this.btnSetting.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_settting_32;
+            this.btnSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.Location = new System.Drawing.Point(-2, 73);
+            this.btnSetting.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnSetting.Size = new System.Drawing.Size(205, 40);
+            this.btnSetting.TabIndex = 2;
+            this.btnSetting.Text = "Cài đặt";
+            this.btnSetting.UseVisualStyleBackColor = false;
+            this.btnSetting.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button1
+            // btnAccount
             // 
-            this.button1.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_profile_32;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(-2, 28);
-            this.button1.Margin = new System.Windows.Forms.Padding(0);
-            this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.button1.Size = new System.Drawing.Size(205, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Tài khoản";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAccount.Enabled = false;
+            this.btnAccount.Image = global::QuanLyQuanCafe.Properties.Resources.fSetting_icon_profile_32;
+            this.btnAccount.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAccount.Location = new System.Drawing.Point(-2, 28);
+            this.btnAccount.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAccount.Name = "btnAccount";
+            this.btnAccount.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnAccount.Size = new System.Drawing.Size(205, 40);
+            this.btnAccount.TabIndex = 0;
+            this.btnAccount.Text = "Tài khoản";
+            this.btnAccount.UseVisualStyleBackColor = true;
+            this.btnAccount.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button3
+            // btnClose
             // 
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button3.Location = new System.Drawing.Point(162, -1);
-            this.button3.Margin = new System.Windows.Forms.Padding(0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(41, 29);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "X";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClose.Location = new System.Drawing.Point(162, -1);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(41, 29);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lbAcccountName
+            // 
+            this.lbAcccountName.AutoSize = true;
+            this.lbAcccountName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbAcccountName.Location = new System.Drawing.Point(67, 4);
+            this.lbAcccountName.Name = "lbAcccountName";
+            this.lbAcccountName.Size = new System.Drawing.Size(65, 24);
+            this.lbAcccountName.TabIndex = 7;
+            this.lbAcccountName.Text = "label1";
             // 
             // fSetting
             // 
@@ -121,29 +140,33 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(202, 258);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbAcccountName);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btLogout);
+            this.Controls.Add(this.btnLanguage);
+            this.Controls.Add(this.btnSetting);
+            this.Controls.Add(this.btnAccount);
             this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "fSetting";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "fSetting";
+            this.Load += new System.EventHandler(this.fSetting_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnAccount;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Button btnLanguage;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btLogout;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lbAcccountName;
     }
 }
